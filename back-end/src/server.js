eb2d5f7c382c7ad7598ae002c37e261ddeb9fbfa
kiran7naam <135760486+kiran7naam@users.noreply.github.com>
2024-03-31 23:42:29 -0400

    back end files

---
+++ back-end/src/server.js
@@ -0,0 +1,64 @@
+import express from 'express';
+const { MongoClient } = require('mongodb');
+
+async function start(){
+        const url = `mongodb://localhost:27017/vue_ecommerce`;
+        const client = new MongoClient(url);
+        
+        await client.connect();
+        const db = client.db('vue_ecommerce');
+
+        const app = express();
+        app.use(express.json());
+
+
+        app.get('/products', async(req,res) => {
+            const products = await db.collection('products').find({}).toArray();
+            res.send(products);
+        })
+
+        async function populateCartIds(ids){
+            return Promise.all(ids.map(id => db.collection('products').findOne({id})))   
+
+        }
+
+        app.get('/users/:userId/cart', async(req,res) => {
+            const user = await db.collection('users').findOne({id : req.params.userId});
+            const populatedCart = await populateCartIds(user.cartItems)
+            res.json(populatedCart)
+        })
+        app.get('/products/:productId', async(req,res) => {
+            const productId = req.params.productId;    
+            const product = await db.collection('products').findOne({ id : productId });
+            res.json(product);
+        })
+
+        app.post('/users/:userId/cart', async(req,res) => {
+            const userId = req.params.userId;
+            const productId = req.body.id;
+            await db.collection('users').updateOne({id : userId},{
+                $addToSet : {cartItems : productId}
+            })
+            const user = await db.collection('users').findOne({id : req.params.userId});
+            const populatedCart = await populateCartIds(user.cartItems)
+            res.json(populatedCart)
+        })
+
+        app.delete('/users/:userId/cart/:productId', async(req,res) => {
+            const userId = req.params.userId;
+            const productId = req.params.productId;
+            await db.collection('users').updateOne({id : userId},{
+                $pull : {cartItems : productId}
+            })
+
+            const user = await db.collection('users').findOne({id : req.params.userId});
+            const populatedCart = await populateCartIds(user.cartItems)
+            res.json(populatedCart)
+
+        })
+        app.listen(8000, () => {
+            console.log("Server is running on port 8000")
+
+        })
+}
+start();
---
+++ back-end/src/temp-data.js
@@ -0,0 +1,60 @@
+// import firstGuitar from './assets/firstguitar.jpg';
+// import secondGuitar from './assets/secondguitar.jpg';
+// import thirdGuitar from './assets/thirdguitar.jpg';
+// import fourthGuitar from './assets/fourthguitar.jpg';
+// import fifthGuitar from './assets/fifthguitar.jpg';
+// import sixthGuitar from './assets/sixthguitar.jpg';
+// import lastGuitar from './assets/lastguitar.jpg';
+
+export const cartItems  = ['1','2','3'];
+
+export const products  = [{
+        "id" : '1',
+        "product_name" : "Squier Guitar",
+        "product_description": "This are one of several Squier models available that offer a pretty good product for a reasonably low price. For the price, these are a very good beginner guitar choice.",
+        "price": "$204.99",
+         "image_Url": './images/firstguitar.jpg',
+    },
+    {
+        "id" : '2',
+        "product_name" : "Gibson SG Guitar",
+        "product_description": "Modelled after the much more expensive Gibson SG guitars, the Epiphone SG G310 keeps it's cost low by using cheaper hardware and lower quality humbucking pick-ups. ",       
+        "price": "$249.00",
+        "image_Url": './images/secondguitar.jpg',
+    },
+    {
+        "id" : '3',
+        "product_name" : "Pacifica Guitar",
+        "product_description": "Here's another guitar many people feel is a great value. This Pacifica features an agathis body, maple neck, and rosewood fretboard, with two single coil pick-ups, and one humbucker.",        
+        "price": "$179.99",
+        "image_Url": './images/thirdguitar.jpg',
+    },
+    {
+        "id" : '4',
+        "product_name" : "Beginner Guitar",
+        "product_description": "Guitarists like Keith Richards, Steve Cropper, Albert Lee, and Danny Gatton favor the look and sound of the Telecaster. If you're a fan of any of those guitarists, this beginner guitar is for you. ",       
+        "price": "$150.00",
+        "image_Url": './images/fourthguitar.jpg',
+    },
+    {
+        "id" : '5',
+        "product_name" : "Rock & Roll Guitar",
+        "product_description": "The Les Paul is perhaps the most famous guitar in rock and roll. Epiphone has done a good job of visually re-creating the Les Paul in this lower-cost guitar marketed towards beginners. ",       
+        "price": "$125.50",
+        "image_Url": './images/fifthguitar.jpg',
+    },
+    {
+        "id" : '6',
+        "product_name" : "Terrific Guitar",
+        "product_description": "These Canadian-made instruments are terrific guitars - very highly regarded, for beautiful sound, and excellent value.  The S6 features a solid cedar top, and mahogany back and sides. ",       
+        "price": "$419.99",
+        "image_Url": './images/sixthguitar.jpg',
+    },
+    {
+        "id" : '7',
+        "product_name" : "Acoustic Guitar",
+        "product_description": "Perhaps not in the same league as the Seagull, the FG700S is still a solid beginner acoustic guitar, and for the price (significantly cheaper than the S6), it's a good value.",       
+        "price": "$199.99",
+        "image_Url": './images/lastguitar.jpg',
+    }
+]
